50337e308122cd42cc86860113c9aee63d05faae
cealmees <cealmees@outlook.com>
2017-02-07 22:28:46 -0600

    Displaying info from local db file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BandDetailComponent } from './band-detail.component';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BandDetailComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     FormsModule,
     HttpModule
